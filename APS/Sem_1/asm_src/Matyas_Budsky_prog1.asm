9419d73c2cb27f99024ea3560d93a02455e9a4e2
Matyáš Budský <budskmat@fit.cvut.cz>
2023-11-17 16:38:12 +0100

    aps: Finnish the main assembly program

--- APS/Sem_1/asm_src/Matyas_Budsky_prog1.asm
+++ APS/Sem_1/asm_src/Matyas_Budsky_prog1.asm
@@ -9,8 +9,19 @@ array:
 
 .text
 #---------------------------------------------------------------------------------------------
-start:
-	
+# main
+lw s0, 8(x0)		# s0 points and iterates through the array
+lw s1, 4(x0)		# s1 holds the array size
+addi s2, zero, 0	# s2 is the array iterator
+
+main_loop:
+	beq s1, s2, end		# break the loop if the array has been iterated through
+	lw a0, 0(s0)
+	jal prime
+	sw a0, 0(s0)
+	addi s2, s2, 1		# increment one
+	addi s0, s0, 4
+	j main_loop
 	
 
 # function checks if number at a0 is prime
@@ -18,11 +29,12 @@ start:
 prime:
 	addi t0, zero, 2	# t0 is an iterator 
 	blt a0, t0, prime_0	# if the number is smaller than 2, it is automatically not prime
+	beq a0, t0, prime_1	# if the number is 2, it is prime
 	prime_loop:
 		rem t1, a0, t0
-		beq t1, zero, prime_1	# if modulo of a0 and t0 is 0, number is prime
-		beq a0, t1, prime_0	# end the cycle -- number is not prime
+		beq t1, zero, prime_0	# if modulo of a0 and t0 is 0, number is not prime
 		addi t0, t0, 1		# increment one
+		beq a0, t0, prime_1	# end the cycle -- number is prime
 		j prime_loop
 	
 	
@@ -33,3 +45,6 @@ prime:
 	prime_1:
 		addi a0, zero, 1
 		ret
+
+end:
+beq x0, x0, end
