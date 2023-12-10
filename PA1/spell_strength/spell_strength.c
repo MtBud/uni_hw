7749a55526586a86ac766b24406132e02cc2638f
Matyáš Budský <budskmat@fit.cvut.cz>
2023-12-10 17:54:30 +0100

    spells: Implement second dictionary for better performance

--- PA1/spell_strength/spell_strength.c
+++ PA1/spell_strength/spell_strength.c
@@ -18,22 +18,37 @@ struct strArr{
 };
 
 /**
+ * Structure representing one word in a dictionary
+ */
+struct wordStr{
+    char* word;
+    int idx;
+};
+
+/**
  * Structure used to store separated whole words
  */
 struct dictionary{
-    char** word;
     size_t max;
     size_t size;
+    struct wordStr* word;
 };
 
-
+/**
+ * Function prints the words in a dictionary
+ * @param dict
+ */
 void printDictionary( struct dictionary* dict ){
     printf("\nDICTIONARY\n");
     for( uint i = 0; i < dict->size; i++ ){
-        printf("%s\n", dict->word[i]);
+        printf("%s\n", dict->word[i].word);
     }
 }
 
+/**
+ * Function transforms a string to lowercase
+ * @param str
+ */
 void strToLower( char* str ){
     int i = 0;
     while( str[i] != '\0' ){
@@ -61,17 +76,29 @@ size_t reallocStr( struct strArr* inStr ){
     return sizediff;
 }
 
+/**
+ * Function used to reallocate the words array in dictionary structure
+ * @param dict
+ */
 void reallocDict( struct dictionary* dict ){
-    //printDictionary( dict );
-    //printf("REALLOC DICT\n");
-    char** tmp;
+    struct wordStr* tmp;
     dict->max *= 2;
-    tmp = (char**) realloc(dict->word, dict->max * sizeof(char*));
+    tmp = (struct wordStr*) realloc(dict->word, dict->max * sizeof(struct wordStr));
     if( tmp == NULL){
         printf("Reallocation failed\n");
         exit (EXIT_FAILURE);
     }
     dict->word = tmp;
+}
+
+/**
+ * Function used for freeing the dictionary structure
+ * @param dict
+ */
+void freeDict( struct dictionary* dict ){
+    for( uint i = 0; i < dict->size ; i ++ )
+        free( dict->word[i].word );
+    free(dict->word);
 }
 
 /**
@@ -81,38 +108,33 @@ void reallocDict( struct dictionary* dict ){
  * @return
  */
 int compareDict( const void* a, const void* b ){
-    const char *arg1 = *(char**)a;
-    const char *arg2 = *(char**)b;
-    char *arg1First = (char*) malloc(sizeof(char) * (strlen(arg1) + strlen(arg2) + 1));
-    char *arg2First = (char*) malloc(sizeof(char) * (strlen(arg1) + strlen(arg2) + 1));
-    strcpy(arg1First, arg1);
-    strcpy(arg2First, arg2);
+    struct wordStr *arg1 = (struct wordStr*) a;
+    struct wordStr *arg2 = (struct wordStr*) b;
+    char *arg1First = (char*) malloc(sizeof(char) * (strlen(arg1->word) + strlen(arg2->word) + 1));
+    char *arg2First = (char*) malloc(sizeof(char) * (strlen(arg1->word) + strlen(arg2->word) + 1));
+    strcpy(arg1First, arg1->word);
+    strcpy(arg2First, arg2->word);
 
     // check for duplicates
-    strToLower( arg1First );
-    strToLower( arg2First );
     if( strcmp(arg1First, arg2First) == 0 ){
         duplicates_flag = true;
     }
 
-    strcat(arg1First, arg2);
-    strcat(arg2First, arg1);
-    strToLower( &arg1First[strlen(arg1)] );
-    strToLower( &arg2First[strlen(arg2)] );
-
+    strcat(arg1First, arg2->word);
+    strcat(arg2First, arg1->word);
 
     int comparison = strcmp(arg1First, arg2First);
-    //printf("compare: %s, %s, outcome: %d\n", arg1, arg2, comparison );
+    if( comparison == 0 ){
+        if( strlen(arg1->word) < strlen(arg2->word)){
+            comparison = -1;
+        }
+        else
+            comparison = 1;
+    }
 
     free(arg1First);
     free(arg2First);
     return comparison;
-}
-
-void freeDict( struct dictionary* dict ){
-    for( uint i = 0; i < dict->size ; i ++ )
-        free( dict->word[i] );
-    free(dict->word);
 }
 
 /**
@@ -157,7 +179,7 @@ int input( struct strArr* inStr ){
  * Separates the one big string into single words and stores them in an array
  * @return 0 on success, 1 on fail ( word is duplicate )
  */
-int separate( struct strArr* inStr, struct dictionary* dict ){
+int separate( struct strArr* inStr, struct dictionary* dict, struct dictionary* dictLwr ){
     char* wordStart = inStr->str;
     char* wordEnd;
     size_t length;
@@ -181,26 +203,31 @@ int separate( struct strArr* inStr, struct dictionary* dict ){
             wordEnd ++;
         }
 
-        // reallocate dictionary if needed
+        // reallocate dictionaries if needed
         if( dictIter == dict->max ){
             dict->size = dictIter;
+            dictLwr->size = dictIter;
             reallocDict( dict );
+            reallocDict( dictLwr );
         }
 
-        // add new word into the dictionary
+        // add new word into the dictionaries
         dict->size = dictIter;
         tmp = wordEnd[0];
         wordEnd[0] = '\0';
         length = strlen(wordStart);
-        dict->word[dictIter] = (char*) malloc( (length+1) * sizeof(char) );
-        strcpy(dict->word[dictIter], wordStart);
-        //printf("OG: %s\n", wordStart);
-        //printf("CP: %s\n", dict->word[dictIter]);
-        //printDictionary(dict);
+        dict->word[dictIter].word = (char*) malloc( (length+1) * sizeof(char) );
+        dictLwr->word[dictIter].word = (char*) malloc( (length+1) * sizeof(char) );
+        strcpy(dict->word[dictIter].word, wordStart);
+        strcpy(dictLwr->word[dictIter].word, wordStart);
+        strToLower(dictLwr->word[dictIter].word);
+        dict->word[dictIter].idx = (int) dictIter;
+        dictLwr->word[dictIter].idx = (int) dictIter;
+
+
         wordEnd[0] = tmp;
         wordStart = wordEnd;
         dictIter ++;
-
     }
 
     // check for no words
@@ -208,6 +235,7 @@ int separate( struct strArr* inStr, struct dictionary* dict ){
         return 1;
 
     dict->size = dictIter;
+    dictLwr->size = dictIter;
 
     return 0;
 }
@@ -217,36 +245,35 @@ int separate( struct strArr* inStr, struct dictionary* dict ){
  * @param dict
  * @return
  */
-int printFinal( struct dictionary* dict ){
+int printFinal( struct dictionary* dict, struct dictionary* dictLwr ){
     uint len = 0, currLen;
 
-    for( int i = (int) dict->size - 1; i >= 0; i -- ){
-        currLen = strlen( dict->word[i] );
+    for( int i = (int) dictLwr->size - 1; i >= 0; i -- ){
+        currLen = strlen( dictLwr->word[i].word );
 
         if( len == 0 ){
-            printf("%s", dict->word[i]);
+            printf("%s", dict->word[dictLwr->word[i].idx].word);
             len = currLen;
             continue;
         }
 
         if( len + currLen >= 80 ){
             len = currLen;
-            printf("\n%s", dict->word[i]);
+            printf("\n%s", dict->word[dictLwr->word[i].idx].word);
             continue;
         }
 
-        printf(" %s", dict->word[i]);
+        printf(" %s", dict->word[dictLwr->word[i].idx].word);
         len += currLen + 1;
     }
     printf("\n");
     return 0;
 }
 
-/*
- * TODO
- * - separate() function, that separates the string into words
- * - function to connect the words for highest lexicographical score - use strcmp()
- * */
+/**
+ * Main function
+ * @return
+ */
 int main(){
     struct strArr inStr;
     inStr.max = STRSIZE;
@@ -260,44 +287,44 @@ int main(){
         free(inStr.str);
         return 1;
     }
-    /*
-    printf("STRING\n");
-    printf("%s\n", inStr.str);
-    */
 
+    // make two dictionaries
+    // dict contains all the input words in original format
+    // dictLwr contains all words in lowercase
     struct dictionary dict;
     dict.max = DICTSIZE;
     dict.size = 0;
-    dict.word = (char**) malloc( dict.max * sizeof(char*) );
+    dict.word = (struct wordStr*) malloc( dict.max * sizeof(struct wordStr) );
+    struct dictionary dictLwr;
+    dictLwr.max = DICTSIZE;
+    dictLwr.size = 0;
+    dictLwr.word = (struct wordStr*) malloc( dict.max * sizeof(struct wordStr) );
 
     // separates the input and fills words into the dictionary
-    if( separate( &inStr, &dict ) ){
+    if( separate( &inStr, &dict, &dictLwr) ){
         printf("Nespravny vstup.\n");
         free(inStr.str);
         freeDict(&dict);
+        freeDict(&dictLwr);
         return 1;
     }
 
     // free the long string because it's no longer needed
     free(inStr.str);
-    //printDictionary( &dict );
 
     // sort the dictionary lexicographically and check for duplicates
-    qsort( dict.word, dict.size, sizeof(char*), compareDict );
+    qsort( dictLwr.word, dict.size, sizeof(struct wordStr), compareDict );
     if( duplicates_flag ){
         printf("Nespravny vstup.\n");
         freeDict(&dict);
+        freeDict(&dictLwr);
         return 1;
     }
 
-    /*
-    printf("\nSORTED\n");
-    printDictionary( &dict );
-    */
-
     // print the output
-    printFinal( &dict );
+    printFinal( &dict, &dictLwr );
 
+    freeDict(&dictLwr);
     freeDict(&dict);
     return 0;
 }
