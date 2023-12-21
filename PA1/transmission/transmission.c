23ca618b8bf82b85e167a1820bbd7c0bed839c75
Matyáš Budský <budskmat@fit.cvut.cz>
2023-12-21 17:18:15 +0100

    transmission: Delete dynamic allocation, add more input checks

--- PA1/transmission/transmission.c
+++ PA1/transmission/transmission.c
@@ -14,17 +14,27 @@ struct s_transmission{
  * Represents an array of s_transmission structures
  */
 struct s_transArr{
-    struct s_transmission* array;
+    struct s_transmission array[50];
     int size;
-    int max;
 };
+
+/**
+ * Prints the given array
+ */
+void printArr( struct s_transArr* transArr ){
+    printf("PRINT ARRAY\n");
+    for( int i = 0; i < transArr->size; i++ ){
+        printf("%d:%d\n", transArr->array[i].num1, transArr->array[i].num2 );
+    }
+}
 
 /**
  * Function takes care of input control and adding data to the array
  * @param type
  * @param transArr
  * @param request
- * @return
+ * @return -1 if EOF occurred, 1 if wrong input has been detected, 0 if addition has been executed, -2 if request
+ * has been detected
  */
 int input( bool* type, struct s_transArr* transArr, struct s_transmission* request ){
     int queryID = getchar();
@@ -58,46 +68,76 @@ int input( bool* type, struct s_transArr* transArr, struct s_transmission* reque
 
     // add new entry to array
     if( queryID == '+' ){
-        transArr->size ++;
-        // reallocate if needed
-        if( transArr->size == transArr->max ){
-            transArr->max = transArr->max * 2;
-            struct s_transmission* tmp = (struct s_transmission*) realloc( transArr->array ,transArr->max * sizeof(struct s_transmission) );
-            if( tmp == NULL ){
-                printf("Out of memory\n");
-                exit(EXIT_FAILURE);
-            }
-            transArr->array = tmp;
+        if( num1 < 10 || num1 > 1000 || num2 < 10 || num2 > 1000 ){
+            printf("ERROR: Numbers out of range\n");
+            return 1;
         }
-        struct s_transmission tmp_str;
-        tmp_str.num1 = num1;
-        tmp_str.num2 = num2;
-        transArr->array[transArr->size - 1] = tmp_str;
+        transArr->array[transArr->size].num1 = num1;
+        transArr->array[transArr->size].num2 = num2;
+        transArr->size ++;
         return 0;
     }
 
+    if( num1 <= 0 || num2 <= 0 ){
+        printf("ERROR: Numbers out of range\n");
+        return 1;
+    }
     request->num1 = num1;
     request->num2 = num2;
-    return 0;
+    return -2;
 }
 
 /**
- * Prints the given array
+ * Get gcd of two numbers
+ * @param a
+ * @param b
+ * @return
  */
-void printArr( struct s_transArr* transArr ){
-    printf("PRINT ARRAY\n");
-    for( int i = 0; i < transArr->size; i++ ){
-        printf("%d:%d\n", transArr->array[i].num1, transArr->array[i].num2 );
-    }
+int gcd(int a, int b){
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+/**
+ * Simplifies a fraction
+ * @param fr1
+ */
+void simplifyFraction( struct s_transmission* fr1 ){
+    int gcdFr = gcd( fr1->num1, fr1->num2 );
+    fr1->num1 = fr1->num1 / gcdFr;
+    fr1->num2 = fr1->num2 / gcdFr;
+}
+
+/**
+ * Multiplies two fractions
+ * @param fr1
+ * @param fr2
+ * @return
+ */
+struct s_transmission multiplyFractions( struct s_transmission fr1, struct s_transmission fr2 ){
+    struct s_transmission frOut;
+    frOut.num1 = fr1.num1 * fr2.num1;
+    frOut.num2 = fr1.num2 * fr2.num2;
+    return frOut;
+}
+
+/**
+ * Finds the closest combination of transmissions from the given array
+ * @return
+ */
+int findTrans( struct s_transArr* transArr, struct s_transmission* request ){
+    simplifyFraction( request );
+    if( request->num1 == 1 && request->num2 == 1 )
+        return 0;
+    return 0;
 }
 
 int main(){
     bool type = false;
     struct s_transmission request;
     struct s_transArr transArr;
-    transArr.max = 2;
     transArr.size = 0;
-    transArr.array = (struct s_transmission*) malloc( transArr.max * sizeof(struct s_transmission) );
 
     printf("Prevody:\n");
     while( true ){
@@ -106,15 +146,16 @@ int main(){
             break;
         if( inputOut == 1 ){
             printf("Nespravny vstup.\n");
-            free( transArr.array );
             exit(EXIT_FAILURE);
         }
 
+        if( inputOut == -2 ){
+            findTrans( &transArr, &request );
+        }
 
 
     }
 
     printArr( &transArr );
-    free( transArr.array );
     exit(EXIT_SUCCESS);
 }
